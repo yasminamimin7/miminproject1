58d347a83b277726c190309cd150eb22d51d88db
yasminamimin7 <yasminasyarifah393@gmail.com>
2026-05-19 11:27:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 st.title("🎈 mimin project 1")
+st.header("Bagaimana pengalamanmu menggunakan website ini?")
+import streamlit as st
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+
 
 st.markdown("*Streamlit* is **really** ***cool***.")
 st.markdown('''
